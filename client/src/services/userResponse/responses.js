ca70fec50b6c2b9b7a59106b93204338a4bad591
Samuel Billaud <samuel.billaud1990@gmail.com>
2018-07-30 13:48:31 +0200

    Add responses.js => data mock

---
+++ client/src/services/userResponse/responses.js
@@ -0,0 +1,21 @@
+export const responses = {
+  'title': 'AB Tasty Dashboard',
+  'responses': [
+    {
+      note: 0,
+      comment: 'Nul !'
+    },
+    {
+      note: 10,
+      comment: 'Je suis hyper content !'
+    },
+    {
+      note: 5,
+      comment: 'Tu bluffes Martoni !'
+    },
+    {
+      note: 7,
+      comment: 'J\'adore ce site, merci'
+    }
+  ]
+}
