2009e7a6b132a94f6d2764a61890c0770e2a32e6
Samuel Billaud <samuel.billaud1990@gmail.com>
2018-07-30 20:19:15 +0200

    Add ListItem component

---
+++ client/src/components/ListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{ comment }}</p>
+    <p>{{ note }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    comment: {
+      type: String,
+      required: true
+    },
+    note: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
