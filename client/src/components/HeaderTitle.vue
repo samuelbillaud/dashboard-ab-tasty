6cc3496ff8574761751a30a85641b6e1915bf355
Samuel Billaud <samuel.billaud1990@gmail.com>
2018-07-30 19:49:55 +0200

    Add HeaderTitle component, just rendering title

---
+++ client/src/components/HeaderTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderTitle',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
